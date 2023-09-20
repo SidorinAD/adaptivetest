71ef69e63891c9e68661b131b887c0e3e968fac8
SidorinAD <57830895+SidorinAD@users.noreply.github.com>
2023-09-20 17:12:37 +0300

    base fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/adaptivetest/',
+  base: '/',
   plugins: [svgr(), react()],
   server: {
     host: true,
