8685af789de4b37eb70086909b4c8d4c7834946b
SidorinAD <57830895+SidorinAD@users.noreply.github.com>
2023-09-20 17:43:44 +0300

    final github pages deploy fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Layout } from "./shared/ui/layout/layout";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/adaptivetest" element={<Layout />}>
         <Route index element={<MainPage />} />
       </Route>
     </Routes>
